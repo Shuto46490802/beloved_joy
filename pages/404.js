3a7204feddcd6a9500826902a064b211deaa52e9
shutosuganuma <shuto@shutos-MacBook-Air.local>
2021-06-17 22:39:02 -0400

    updated

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Head from 'next/head';
 
-import error from "../assets/error.jpg";
+import error from "../assets/error.jpg"
 
 export const getStaticProps = async () => {
 
